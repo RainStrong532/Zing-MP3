0ef82b91d7828d0d518c91b2cc98b13fe9c78c10
manh van vu <vuvanmanhttvtvp@gmail.com>
2020-05-04 10:59:18 +0700

    delete firebase

--- public/js/backToTop.js
+++
@@ -1,7 +0,0 @@
-const back_to_top = document.querySelector('.back_to_top');
-
-back_to_top.addEventListener('click', () => {
-    console.log("A", window.scrollY);
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-})
